910ef81ffd769c7eff274f17855882fd50c0bdbc
a17100262 <maueezahmed@gmail.com>
2018-08-04 11:43:21 +0000

    S3 details added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,4 +112,14 @@ Rails.application.configure do
     :password => ENV['mailer_password'],
     :authentication => :plain
     }
+  
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV['S3_BUCKET_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      s3_region: ENV['AWS_REGION'],
+    }
+  }
 end
