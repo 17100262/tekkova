61270ddaeeb4e09460a2f9ac3343f48c6c2752fd
a17100262 <maueezahmed@gmail.com>
2019-01-10 13:53:00 +0000

    db reset

--- db/migrate/20181113091627_add_offers_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddOffersToUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :send_updates, :boolean
-    add_column :users, :offers, :boolean,default: false
-  end
-end
