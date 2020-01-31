1ea2a877c20bf0fd0bd2a3ae8fe4917b3f136944
Saqib Shahzad <saqib.lums@gmail.com>
2020-01-31 18:20:48 +0500

    Change seeds files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
 
-car_features = ['Front Center Armrests','Rear Center Armrests','Multifunctional Steering Wheel','2 Zone Climate Control','Daytime LED Lights','Traction Control','Blind Spot Sensor','Parking Sensors','Rear View Camera','Starter Button','Cruise Control','Keyless Start',"LED headlamps",'Sports Mode','Cupholders','Bike Rack', 'Heated Seats', 'Sunroof','Bluetooth', 'GPS','Sun Roof','AUX','ABS','Booster Seat','Pet Friendly','USB Input', 'Window Tint', 'Alarm',]
+car_features = ['Front Center Armrests', 'Rear Center Armrests',
+                'Multifunctional Steering Wheel', '2 Zone Climate Control',
+                'Daytime LED Lights', 'Traction Control', 'Blind Spot Sensor',
+                'Parking Sensors', 'Rear View Camera', 'Starter Button',
+                'Cruise Control', 'Keyless Start', 'LED headlamps',
+                'Sports Mode', 'Cupholders', 'Bike Rack', 'Heated Seats',
+                'Sunroof', 'Bluetooth', 'GPS', 'Sun Roof', 'AUX', 'ABS',
+                'Booster Seat', 'Pet Friendly', 'USB Input', 'Window Tint',
+                'Alarm', 'CarPlay', 'Tow Bar', 'Hill holder',
+                'Driver airbag', 'Passenger airbags', 'Central locking',
+                'Sport paddles', 'Storage Compartment/Center console',
+                'Floor mats', 'Lane departure warning', 'Side airbags',
+                'Parking assist', 'Blind Spot Mirrors',
+                'Day time LED running lights']
+
 car_features.each do |feature|
-    CarFeature.create(name: feature)
+  CarFeature.create(name: feature)
 end
