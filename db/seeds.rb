30c7382c1195ebe317e64fda63f4f1c5ac772300
gullu143 <chwaqaspunjabian143@gmail.com>
2020-02-17 13:11:59 +0500

    car feature check boxes responsive

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,9 @@ car_features = ['Front Centre Armrests', 'Rear Centre Armrests',
                 'Booster Seat', 'Pet Friendly', 'USB Input', 'Window Tint',
                 'Alarm', 'CarPlay', 'Tow Bar', 'Hill holder',
                 'Driver airbag', 'Passenger airbags', 'Central locking',
-                'Sport paddles', 'Storage Compartment/Center console',
-                'Floor mats', 'Lane departure warning', 'Side airbags',
+                'Sport paddles','Floor mats', 'Lane departure warning', 'Side airbags',
                 'Parking assist', 'Blind Spot Mirrors',
-                'Day time LED running lights']
+                'Day time LED running lights', 'Storage Compartment/Center console']
 
 CarFeature.all.destroy_all
 
