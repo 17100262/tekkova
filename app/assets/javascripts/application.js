71562097d46dd4ada664f0e6cf4888dddc32d1ae
gullu143 <chwaqaspunjabian143@gmail.com>
2020-08-14 15:33:37 +0500

    changes 5 card

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -367,7 +367,12 @@ $(window).load(function() {
   if (cookie_avert === null) {
     banner_text = 'Please use a PC, laptop or a tablet instead of a mobile phone to signup, list your vehicle or if you are simply just browsing Tekkova, in order to have a smoother experience. Thank you. <button class="btn btn-success btn-gradient btn-sm" id="accept-cookie"><i class="fas fa-check"></i></button>';
     $("body").prepend('<div id="cookies-banner" class="alert alert-info pr-5">' + banner_text + '</div>');
+    $(".corona-banner").hide();
     $("#accept-cookie").click(function() {
+      $(".corona-banner").show();
+      $('.coron-btn').click(function(){
+         $(".corona-banner").remove();
+      })
       id_button = $(this).attr("id");
       action_button = (id_button == "accept-cookie") ? 'set' : 'not';
       createCookie("cookie_avert", action_button, 365);
@@ -381,6 +386,7 @@ $(window).load(function() {
     setTimeout(function() {
       createCookie("cookie_avert", 365);
       $("#cookies-banner").slideUp(350).remove();
+      $(".corona-banner").show();
       $.ga.load(g_analytics_id, function(pageTracker) {
         pageTracker._setDomainName(host);
       });
@@ -419,3 +425,10 @@ $('.select-state').select(function() {
     $('#selected-state').val($(this).val());
 });
 });
+
+
+
+  $(document).ready(function () {
+      
+      $(".corona-banner").hide();  
+      });  
